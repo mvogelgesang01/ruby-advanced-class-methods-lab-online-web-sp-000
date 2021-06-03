34737e5c4cf06bcd99d2c109b5d22c05b82a8ed6
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-06-03 20:31:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,4 +31,12 @@ end
 
 def self.find_by_name(name)
 @@all.detect{ |song| song.name == name}
+
+
+def self.find_or_create_by_name(name)
+  if self.find_by_name(name)
+    self.find_by_name(name)
+  else
+    self.create_by_name(name)
+  end  
 end
