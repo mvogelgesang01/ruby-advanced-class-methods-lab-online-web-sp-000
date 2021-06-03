910a1816c6dabb79331a0fe336b152eab10554df
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-06-03 21:06:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,13 @@ def self.find_or_create_by_name(name)
     song.artist_name = split_filename[0]
     song
 end
+
+def self.create_from_filename(filename)
+  split_filename = filename.chop.chop.chop.chop.split(" - ")
+  song = Song.new
+  song.name = split_filename[1]
+  song.artist_name = split_filename[0]
+  song.save
+  song
+end
 end
